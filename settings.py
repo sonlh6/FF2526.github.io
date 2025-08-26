098bcac28c3291f51719c1e3f51a300d3740c35f
sonlh6 <sonhongdt@gmail.com>
2025-08-26 23:52:39 +0700

    Define BASE_DIR in settings

--- settings.py
+++ settings.py
@@ -4,7 +4,8 @@ import os
 import dj_database_url
 from pathlib import Path
 
-# ... (các phần khác)
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent
 
 # Lấy SECRET_KEY từ biến môi trường
 SECRET_KEY = os.environ.get('SECRET_KEY')
